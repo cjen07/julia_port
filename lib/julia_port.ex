a136387432145c0c56c03c8dbcd171fe2668ce8c
Wang Chen <wangcjen@gmail.com>
2017-02-27 13:06:44 -0600

    Add files via upload

--- lib/julia_port.ex
+++ lib/julia_port.ex
@@ -12,31 +12,41 @@ defmodule JuliaPort do
   end
 
   def simple_test(port) do
-    port_send("1+2", port)
-    port_receive(true)
+    port_send(port, "1+2")
+    port_receive(port, true)
   end
 
   def complex_test(port) do
     rand(port, :a, 3, 3)
     rand(port, :b, 3, 3)
     JuliaPort.*(port, :c, :a, :b)
-    port_receive()
+    port_receive(port, false)
     sum(port, :d, :c)
-    port_receive(true)
+    port_receive(port, true)
   end
 
-  defp port_send(command, port) do
+  defp port_send(port, command) do
     send port, {self(), {:command, command <> "\n"}}
   end
 
-  defp port_receive(verbose? \\ false) do
+  defp port_receive(port, verbose?) do
+    port_send(port, ":f")
+    loop(verbose?, "")
+  end
+
+  defp loop(verbose?, data) do
     receive do
-      {_pid, {:data, data}} ->
-        if verbose?, do: Logger.debug "received data: #{inspect(data)}"
-        port_receive()
+      {_pid, {:data, raw}} ->
+        data_new = String.trim(raw)
+        cond do
+          String.contains? data_new, "f" ->
+            if verbose?, do: "received data: " <> data
+          data_new == ":" or data_new == "" -> 
+            loop(verbose?, data)
+          true ->
+            loop(verbose?, data <> data_new)
+        end
       _ -> raise "receive error"
-    after
-      2000 -> :ok
     end
   end
 end
